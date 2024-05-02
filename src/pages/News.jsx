aeb1e05884a579e1f1e10b710a86f746b7b51ef8
akyarv0 <akyarv0@gmail.com>
2024-05-02 23:05:17 +0300

    first commit

---
+++ src/pages/News.jsx
@@ -0,0 +1,62 @@
+import Box from "@mui/material/Box"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { CardMedia } from "@mui/material"
+import { useEffect } from "react"
+import { getNews } from "../features/newsSlice"
+import { useDispatch, useSelector } from "react-redux"
+import loadingGif from "../assets/loading.gif"
+
+const News = () => {
+  const dispatch = useDispatch()
+
+  const { newsData , loading , error} = useSelector((state) => state.news)
+
+  useEffect(() => {
+    dispatch(getNews())
+  }, [])
+
+  return (
+    <>
+      <h1>NEWS</h1>
+      {loading && <img src={loadingGif} alt="loading"/>}
+      <Box
+        xs={{ d: "flex" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-evenly"
+        flexWrap="wrap"
+      >
+        {newsData?.map((item, index) => (
+          <Card sx={{ maxWidth: 345, m: 5, maxHeight: 600 }} key={index}>
+            <CardMedia
+              component="img"
+              height="250"
+              image={item?.urlToImage || "https://source.unsplash.com/random"}
+              alt="img"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item?.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {item?.content}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small" href={item?.url} target="_blank">
+                Detail
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Box>
+    </>
+  )
+}
+
+export default News
